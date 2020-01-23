9822cabf31c575a5ee472a3a5a21e36842bba515
tkillen <57112401+tnkillen@users.noreply.github.com>
2020-01-23 09:10:46 -0600

    Installed Sharpnado

---
+++ ZestHealthApp/ZestHealthApp/GradientColorPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Xamarin.Forms;
+namespace ZestHealthApp.Views
+{
+    public partial class GradientColorPage : ContentPage
+    {
+        public GradientColorPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ZestHealthApp/ZestHealthApp/GradientColorStack.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+namespace ZestHealthApp
+{
+    public class GradientColorStack : StackLayout
+    {
+        public Color StartColor
+        {
+            get;
+            set;
+        }
+        public Color EndColor
+        {
+            get;
+            set;
+        }
+    }
+}
