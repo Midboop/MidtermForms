f48f14f104a9fc5538237ec73bb6e4c04161b99a
dtomei13 <57144894+dtomei13@users.noreply.github.com>
2020-01-31 20:53:38 -0700

    still working...

--- ZestHealthApp/ZestHealthApp/RegistrationPage.xaml.cs
+++ ZestHealthApp/ZestHealthApp/RegistrationPage.xaml.cs
@@ -5,7 +5,10 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Acr.UserDialogs;
+using firebasesample.Services.FirebaseAuth;
+using firebasesample.ViewModels.Base;
+using firebasesample.ViewModels.Main;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using ZestHealthApp.Tables;
@@ -20,46 +23,5 @@ namespace ZestHealthApp
             InitializeComponent();
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
-        {
-            var dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "UserDataBase.db");
-            var db = new SQLiteConnection(dbPath);
-            db.CreateTable<RegUserTable>();
-
-            var item = new RegUserTable()
-            {
-                UserName = EntryUserName.Text,
-                Password = EntryPassword.Text,
-                Email = EntryEmail.Text
-
-            };
-
-            db.Insert(item);
-            Device.BeginInvokeOnMainThread(async () =>
-            {
-                //if (EntryUserName.Text == item.UserName)
-                //{
-                //    var result1 = await this.DisplayAlert("Sorry, username in use", "Please try again", "Continue", "Cancel");
-                //    if (result1)
-                //        await Navigation.PushModalAsync(new RegistrationPage());
-                //}
-                //else
-
-                //{
-                    var result = await this.DisplayAlert("Congratulations!", "User Regitration Successful", "Continue", "Cancel");
-                    if (result)
-                    {
-                        await Navigation.PushModalAsync(new LoginPage());
-                    }
-
-                
-              
-            });
-        }
-
-        async private void Button_Clicked_1(object sender, EventArgs e)
-        {
-            await Navigation.PushModalAsync(new LoginPage());
-        }
     }
 }
