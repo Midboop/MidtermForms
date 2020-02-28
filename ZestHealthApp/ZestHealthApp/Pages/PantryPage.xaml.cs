ad499a7ac2e0d3d4130e9682978de725dd8cbb21
tkillen <57112401+tnkillen@users.noreply.github.com>
2020-02-28 17:44:56 -0600

    Added edit and shopping cart buttons
    added to pantry page to appear on a selected item with animation. At this time they only delete.

--- ZestHealthApp/ZestHealthApp/Pages/PantryPage.xaml.cs
+++ ZestHealthApp/ZestHealthApp/Pages/PantryPage.xaml.cs
@@ -22,6 +22,8 @@ namespace ZestHealthApp
         PantryView view;
         PantryItems selectedItem;
         int CurrentFrame;
+        bool CartAnimComplete;
+        bool EditAnimComplete;
         public PantryPage()
         {
             
@@ -31,20 +33,31 @@ namespace ZestHealthApp
             InitializeComponent();
             BindingContext = new PantryView();
             CurrentFrame = 0;
+            CartAnimComplete = false;
+            EditAnimComplete = false;
           
         }
         protected override async void OnAppearing()
         {
             base.OnAppearing();
+            ToggleCartAnimState(false, false);
+            ToggleEditAnimState(false, false);
             await (BindingContext as PantryView).RefreshPantry();
             AnimButton.PlayFrameSegment(0, 25);
             CurrentFrame = 25;
             selectedItem = null;
+
         }
 
         private void CollectionView_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             selectedItem = (e.CurrentSelection.FirstOrDefault() as PantryItems);
+            ToggleCartAnimState(true, false);
+            CartAnimButton.PlayFrameSegment(0, 50);
+
+            ToggleEditAnimState(true, false);
+            EditAnimButton.PlayFrameSegment(0, 14);
+
 
             if (CurrentFrame == 25)
             {
@@ -53,18 +66,46 @@ namespace ZestHealthApp
             }
         }
 
-        private async void Button_Clicked(object sender, EventArgs e)
+        private async void AddDeleteButton_Clicked(object sender, EventArgs e)
         {
             if (CurrentFrame == 25)
                 await Shell.Current.GoToAsync("PantryAddItem");
             else if (CurrentFrame == 45)
             {
                (BindingContext as PantryView).Delete.Execute(selectedItem);
-                AnimButton.PlayFrameSegment(45, 25);
+                AnimButton.PlayFrameSegment(45, 125);
+                CartAnimButton.PlayFrameSegment(50, 102); // use frame 160 if item is added to shopping cart
+                CartAnimComplete = true;
+                EditAnimButton.PlayFrameSegment(14,48);
+                EditAnimComplete = true;
+                AnimButton.PlayFrameSegment(0, 25);
                 CurrentFrame = 25;
                 selectedItem = null;
             }
         }
+        private void ToggleCartAnimState(bool buttonstate, bool complete)
+        {
+            CartButton.IsEnabled = buttonstate;
+            CartAnimButton.IsVisible = buttonstate;
+            CartAnimComplete = complete;
+        }
 
+        private void ToggleEditAnimState(bool buttonstate, bool complete)
+        {
+            EditButton.IsEnabled = buttonstate;
+            EditAnimButton.IsVisible = buttonstate;
+            EditAnimComplete = complete;
+        }
+        private void CartAnimButton_OnPlay(object sender, EventArgs e)
+        {
+            if (CartAnimComplete)
+                ToggleCartAnimState(false, false);
+        }
+
+        private void EditAnimButton_OnPlay(object sender, EventArgs e)
+        {
+            if (EditAnimComplete)
+                ToggleEditAnimState(false, false);
+        }
     }
 }
